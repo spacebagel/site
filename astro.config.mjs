6dfe0ef27d5084348eb50b39ac45ef6eaeff03fd
Vincent 🌿 <165411846+spacebagel@users.noreply.github.com>
2026-05-09 19:26:39 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://spacebagel.github.io/site',
-	baseL '/site',
+	base '/site',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
